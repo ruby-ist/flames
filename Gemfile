40b2c76169fa4a63a73ea92aa268834244b57630
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-11-03 18:58:30 +0530

    Render config

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source "https://rubygems.org"
 
-ruby '2.6.8'
+ruby '2.7.4'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+curl --create-dirs -o $HOME/.postgresql/root.crt -O https://cockroachlabs.cloud/clusters/7ae464c2-e293-4708-957a-373ade476566/cert
